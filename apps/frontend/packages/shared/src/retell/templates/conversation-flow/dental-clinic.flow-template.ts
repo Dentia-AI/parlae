16f0e8fb8c9fb46dd283bae6b04b1b0fb37ddd8f
rafa-9 <rafa.inspired9@gmail.com>
2026-02-28 20:08:49 -0500

    v1.4

--- apps/frontend/packages/shared/src/retell/templates/conversation-flow/dental-clinic.flow-template.ts
+++ apps/frontend/packages/shared/src/retell/templates/conversation-flow/dental-clinic.flow-template.ts
@@ -54,7 +54,7 @@ import {
 // Flow version
 // ---------------------------------------------------------------------------
 
-export const CONVERSATION_FLOW_VERSION = 'cf-v1.3';
+export const CONVERSATION_FLOW_VERSION = 'cf-v1.4';
 
 // ---------------------------------------------------------------------------
 // Config interface for building the flow
